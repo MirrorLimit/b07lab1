c288e1c331f57689c0e4e15ad04ce1874d109f58
MirrorLimit <113953454+MirrorLimit@users.noreply.github.com>
2022-10-01 21:15:36 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,90 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver { 
- public static void main(String [] args) { 
+ public static void main(String [] args) throws FileNotFoundException, IOException { 
   Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
+  System.out.println(p.evaluate(3)); //should get 0
   double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
+  int [] e1 = {1, 2, 3, 4};
+  Polynomial p1 = new Polynomial(c1, e1); 
+  System.out.println("printing p1 below--------------");
+  //System.out.printf(" p1 coefficients length: %d", p1.coefficientsNZ.length);
+  for (int i = 0; i < p1.coefficientsNZ.length; i++)
+  {
+	  System.out.print(p1.coefficientsNZ[i]);
+	  System.out.print(", ");
+	  System.out.print(p1.expo[i]);
+	  System.out.print("; ");
+  }
+  System.out.println();
+  double [] c2 = {1,-2,0,0,-9}; 
+  int [] e2 = {0, 2, 4, 3, 5};
+  Polynomial p2 = new Polynomial(c2, e2); 
+  System.out.println("printing p2 below--------------");
+  for (int i = 0; i < p2.coefficientsNZ.length; i++)
+  {
+	  System.out.print(p2.coefficientsNZ[i]);
+	  System.out.print(", ");
+	  System.out.print(p2.expo[i]);
+	  System.out.print("; ");
+  }
+  System.out.println();
+  Polynomial s = p1.add(p2); //should get -9x^5+5x^4-2x^2+6x+1
+  for (int i = 0; i < s.coefficientsNZ.length; i++)
+  {
+	  System.out.print(s.coefficientsNZ[i]);
+	  System.out.print(", ");
+	  System.out.print(s.expo[i]);
+	  System.out.print("; ");
+  }
+  System.out.println();
   System.out.println("s(0.1) = " + s.evaluate(0.1)); 
   if(s.hasRoot(1)) 
    System.out.println("1 is a root of s"); 
   else 
    System.out.println("1 is not a root of s"); 
+  
+  //testing multiply
+  //p1 * p2 should be -45x^9 - 64x^6 + 5x^4 - 12x^3 + 6x
+  Polynomial product = p1.multiply(p2);
+  System.out.println("printing product now:-------------------------");
+  for (int i = 0; i < product.coefficientsNZ.length; i++)
+  {
+	  System.out.print(product.coefficientsNZ[i]);
+	  System.out.print(", ");
+	  System.out.print(product.expo[i]);
+	  System.out.print("; ");
+  }
+  System.out.println();
+  
+  //testing file constructor with a file with a valid expression, not constant; test A; "3x2-8+5x9"
+  //test A passed
+  //testing file constructor with an empty file; test B; ""
+  //test B passed
+  //testing file constructor with a constant term; test C; "+7"
+  //test C passed
+  File fileTest = new File("C:/Users/Killt/b07lab1/file input testing.txt");
+  Polynomial fileTestPoly = new Polynomial(fileTest);
+  System.out.println("printing test C now:-------------------------");
+  for (int i = 0; i < fileTestPoly.coefficientsNZ.length; i++)
+  {
+	  System.out.print(fileTestPoly.coefficientsNZ[i]);
+	  System.out.print(", ");
+	  //System.out.print(fileTestPoly.expo[i]);
+	  //System.out.print("; ");
+  }
+  System.out.println();
+	 
+  //testing saveToFile with a normal expression; test D; 3x2-8+5x9
+  //test D passed, prints in decimal forms
+  //testing saveToFile with the 0 polynomial; test E; null for both arrays
+  //test E passed
+  //testing saveToFile with a constant term; test F; +7
+  double [] fileTestC = {7.0};
+  int [] fileTestE = {0};
+  fileTestPoly = new Polynomial(fileTestC, fileTestE);
+  fileTestPoly.saveToFile("C:/Users/Killt/b07lab1/file output testing.txt");
  } 
 } 
--- Polynomial.java
+++ Polynomial.java
@@ -1,59 +1,280 @@
+import java.util.Arrays;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial
 {
-	double [] coefficients;
+	//sets the 0 polynomial to null
+	double [] coefficientsNZ;//stores nonzero coefficients
+	int [] expo;//stores the exponents on the nonzero coefficients
+	
 	public Polynomial()
 	{
-		coefficients = new double[1];
-		coefficients[0] = 0.0;
+		coefficientsNZ = null;
+		expo = null;
 	}
 
-	public Polynomial(double [] input)
+	public Polynomial(double [] inputCof, int [] inputExpo)
 	{
-		coefficients = new double[input.length];
-		for(int i = 0; i < input.length; i++)
+		int count = 0;
+		coefficientsNZ = new double[inputCof.length];
+		for(int i = 0; i < inputCof.length; i++)
 		{
-			coefficients[i] = input[i];
+			if (inputCof[i] != 0)// this part actually puts gaps in the array, needs to take care
+			{
+				coefficientsNZ[count] = inputCof[i];
+				count = count + 1;
+			}
 		}
+		coefficientsNZ = Arrays.copyOf(coefficientsNZ, count);//this removes extra 0s
+		count = 0;
+		
+		if (Arrays.binarySearch(coefficientsNZ, 0.0) < 0)//handles 0 polynomial
+		{
+			
+			expo = new int[inputExpo.length];
+			for(int i = 0; i < inputExpo.length; i++)
+			{
+				if (inputCof[i] != 0)//doesn't put in the exponent in case coefficient is 0 to keep length the same
+				{
+					expo[count] = inputExpo[i];
+					count = count + 1;
+				}
+			}
+			expo = Arrays.copyOf(expo, count);
+		}
+		else
+		{
+			coefficientsNZ = null;
+			expo = null;
+		}
+		
+	}
+	
+	public Polynomial(File inputF) throws FileNotFoundException, IOException
+	{
+		//open the file, then parse, add things to the arrays
+		BufferedReader opened = new BufferedReader(new FileReader(inputF));
+		String line = opened.readLine();
+		
+		/*-3x2+5+7x8
+		split with x
+		"-3", "2+5+7", "8"
+
+		-3x2+5+7x8
+		split with +
+		"-3x2", "5", "7x8"
+		split with - on each substring
+		"3x2", "5", "7x8"
+		split with x on each substring
+		"3", "2", "5", "7", "8" THIS PART GETS ALL THE NUMBERS BUT UNSIGNED AND DON'T KNOW IF IT'S COEFFICIENTOF EXPONENENT
+		
+		CAN TRY TO USE indexOf ON ALL THESE SUBSTRINGS STARTING AT THE BEGINNING. AFTER FINDING THE INDEX, LOOK AT THE INDEX RIGHT BEFORE
+		FOR THE SIGN. IF INDEX BEFORE IS OUT OF BOUNDS THEN IT'S POSITIVE, ELSE THEN CHECK TO SEE IF IT'S A COEFFICIENT (+/-) OR IF IT'S AN
+		EXPONENET (X INFRONT OF IT). ADD TO THE CORRESPONDING ARRAYS*/
+		
+		if (line != null)
+		{
+			String [] subs = line.split("\\+"); //this split with a constant term "+7" actually makes {"", "+7}
+			String [] newSubs = new String[0];
+			String [] newSubs2 = new String[0];
+			String [] newSubs3 = new String[0];
+			int index = 0;
+			int fromI = 0;
+			int outCI = 0;
+			int outEI = 0;
+			
+			double [] outCoef = new double [0];
+			int [] outExpo = new int [0];
+			for (String i:subs)
+			{
+				index = newSubs.length;
+				newSubs = Arrays.copyOf(newSubs, (i.split("-").length + newSubs.length));
+				newSubs2 = i.split("-");
+				for (int j = index; j < newSubs.length; j++)//loops to put in the new strings of spliting by "-"
+				{
+					newSubs[j] = newSubs2[j - index];
+				}
+			}//now newSubs should contain numbers and x but no signs. Need to split with x
+			
+			
+			for (String i:newSubs)
+			{
+				index = newSubs3.length;
+				newSubs3 = Arrays.copyOf(newSubs3, (i.split("x").length + newSubs3.length));
+				newSubs2 = i.split("x");
+				for (int j = index; j < newSubs3.length; j++)
+				{
+					newSubs3[j] = newSubs2[j - index];
+				}
+			}//now newSubs should have all numbers. Need to figure out the signs and if they're coefficients or exponenents
+			
+			for (String i:newSubs3)
+			{
+				//newSubs3 should be the one used as it has all the coefficients
+				if (i != "")
+				{
+					index = line.indexOf(i, fromI);
+					if ((index - 1) < 0)//i is the first thing in the string so it must be a coefficient, positive
+					{
+						outCoef = Arrays.copyOf(outCoef, outCoef.length + 1);
+						outCoef[outCI] = Double.parseDouble(i);
+						outCI = outCI + 1;
+						if ((index + 1) < line.length() && line.charAt(index + 1) != 'x')//constant term
+						{
+							outExpo = Arrays.copyOf(outExpo, outExpo.length + 1);
+							outExpo[outEI] = 0;
+							outEI = outEI + 1;
+						}
+					}
+					else if (line.charAt(index - 1) == 'x')//exponent
+					{
+						outExpo = Arrays.copyOf(outExpo, outExpo.length + 1);
+						outExpo[outEI] = Integer.parseInt(i);
+						outEI = outEI + 1;
+					}
+					else if (line.charAt(index - 1) == '+')//+ coefficient
+					{
+						outCoef = Arrays.copyOf(outCoef, outCoef.length + 1);
+						outCoef[outCI] = Double.parseDouble(i);
+						outCI = outCI + 1;
+						if ((index + 1) < line.length() && line.charAt(index + 1) != 'x')//constant term
+						{
+							outExpo = Arrays.copyOf(outExpo, outExpo.length + 1);
+							outExpo[outEI] = 0;
+							outEI = outEI + 1;
+						}
+					}
+					else if (line.charAt(index - 1) == '-')//- coefficient
+					{
+						outCoef = Arrays.copyOf(outCoef, outCoef.length + 1);
+						outCoef[outCI] = Double.parseDouble("-" + i);
+						outCI = outCI + 1;
+						if ((index + 1) < line.length() && line.charAt(index + 1) != 'x')//constant term
+						{
+							outExpo = Arrays.copyOf(outExpo, outExpo.length + 1);
+							outExpo[outEI] = 0;
+							outEI = outEI + 1;
+						}
+					}
+				}
+			}
+			this.coefficientsNZ = outCoef;
+			this.expo = outExpo;
+			opened.close();
+		}
+	}
+	
+	public void saveToFile (String path) throws FileNotFoundException// assumes file is empty and writes to 1 line
+	{
+		//need to go through expo and coef at the same time, put in coefficient first and then
+		//check the exponent, if exponent = 0 than it is a constant, if exponent > 0 then
+		//need to add x in with exponent right after
+		
+		File outfile = new File (path);
+		PrintStream output = new PrintStream(outfile);
+		if (this.coefficientsNZ == null && this.expo == null)
+		{
+			output.print(0);
+			output.close();
+			return;
+		}
+
+		for (int i = 0; i < this.coefficientsNZ.length; i++)
+		{
+			//write the coefficient into the file, check if the exponent is 0 or not and put in
+			//x accordingly
+			if (this.coefficientsNZ[i] > 0)//need to add a detection to see if I need to put in + for the beginning or not
+			{
+				if (i != 0)
+				{
+					output.printf("+");
+				}
+				output.printf("%f", this.coefficientsNZ[i]);
+			}
+			else//this assumes that a negative coefficient is printed with the signs
+			{
+				output.printf("%f", this.coefficientsNZ[i]);
+			}
+			//check for exponents adds them in
+			if (this.expo[i] != 0)
+			{
+				output.printf("%c", 'x');
+				output.printf("%d", this.expo[i]);
+			}
+		}
+		output.close();
+		
 	}
 
 	public Polynomial add(Polynomial input)
 	{
-		Polynomial output;
-		int maxLength = Math.max(input.coefficients.length, coefficients.length);
-		if (maxLength == input.coefficients.length)
+		Polynomial output = new Polynomial(this.coefficientsNZ, this.expo);
+		int indexExpo = -1;
+		int indexThis = -1;
+		int indexIn = -1;
+		
+		if (output.coefficientsNZ == null)
 		{
-			output = new Polynomial(input.coefficients);
+			return input;
+		}
+		else if (input.coefficientsNZ == null)
+		{
+			return output;
 		}
 		else
 		{
-			output = new Polynomial(coefficients);
-		}
-		
-
-		if (maxLength == input.coefficients.length)
-		{
-			for(int i = 0; i < coefficients.length; i++)
+			Arrays.sort(expo);
+			
+			for (int x:input.expo)
 			{
-				output.coefficients[i] = output.coefficients[i] + coefficients[i];
+				//look for x in output, if x isn't in output, add it
+				indexExpo = Arrays.binarySearch(output.expo, x);
+				if (indexExpo < 0 || indexExpo >= output.expo.length)//not found
+				{
+					output.expo = Arrays.copyOf(output.expo, output.expo.length + 1);//copies old expo over with 1 extra place to add in x
+					output.expo[output.expo.length - 1] = x;
+				}
+			}
+			Arrays.sort(output.expo);
+			//exponents are all in
+			output.coefficientsNZ = new double[output.expo.length];
+			
+			for (int i = 0; i < output.expo.length; i++)//loop through output.expo and add the coefficients for the exponent
+			{
+				indexThis = Arrays.binarySearch(expo, output.expo[i]);
+				indexIn = Arrays.binarySearch(input.expo, output.expo[i]);
+				if (indexThis >=0 && indexThis < expo.length)
+				{
+					output.coefficientsNZ[i] = this.coefficientsNZ[indexThis];
+				}
+				if (indexIn >= 0 && indexIn < input.expo.length)
+				{
+					output.coefficientsNZ[i] = output.coefficientsNZ[i] + input.coefficientsNZ[indexIn];
+				}
 			}
 		}
 		
-		else
-		{
-			for(int i = 0; i < input.coefficients.length; i++)
-			{
-				output.coefficients[i] = output.coefficients[i] + input.coefficients[i];
-			}
-		}
 		return output;
 	}
 
 	public double evaluate(double x)
 	{
 		double output = 0.0;
-		for(int i = 0; i < coefficients.length; i++)
+		if (coefficientsNZ == null)
 		{
-			output = coefficients[i] * Math.pow(x, i) + output;
+			return 0.0;
+		}
+		else
+		{
+			for(int i = 0; i < coefficientsNZ.length; i++)
+			{
+				output = coefficientsNZ[i] * Math.pow(x, expo[i]) + output;
+			}
 		}
 		return output;
 	}
@@ -66,4 +287,66 @@ public class Polynomial
 		}
 		return false;
 	}
+	
+	public Polynomial multiply(Polynomial input)
+	{
+		//make a new expo array that stores all the resulting expo
+		//after multiplying, check if the exponent is already in, if not, then add it into the
+		//array. Sort the new expo. Make a new coef array of size new-expo.length. now multiply
+		//again but for the coefficients, make sure the resultant is operated with the new-coef
+		//entry
+		
+		
+		//outExpo shouldn't be multiply as they're exponents, should be add
+		
+		if (this.coefficientsNZ == null || input.coefficientsNZ == null)
+		{
+			Polynomial out = new Polynomial();
+			return out;
+		}
+		
+		boolean first = true;
+		
+		int [] outExpo = null;
+		int index = -1;
+		for (int i = 0; i < this.expo.length; i++)
+		{
+			for (int j = 0; j < input.expo.length; j++)
+			{
+				if (outExpo == null)
+				{
+					outExpo = new int[1];
+					outExpo[0] = this.expo[i] + input.expo[j];
+				}
+				else//this makes it so that if it is null it keeps renewing it
+				{	
+					index = Arrays.binarySearch(outExpo, this.expo[i] + input.expo[j]);
+					first = false;
+				}
+				
+				if (outExpo != null && first == false &&
+						(index < 0 || index >= outExpo.length))
+				{
+						outExpo = Arrays.copyOf(outExpo, outExpo.length + 1);
+						outExpo[outExpo.length - 1] = this.expo[i] + input.expo[j];
+						Arrays.sort(outExpo);
+				}
+			}
+		}//this nested loop should take care of putting together an array of of multiplied exponents
+
+		double [] outCoef = new double[outExpo.length];
+		
+		for (int i = 0; i < this.expo.length; i++)
+		{
+			for (int j = 0; j < input.expo.length; j++)
+			{
+				index = Arrays.binarySearch(outExpo, this.expo[i] + input.expo[j]);
+				outCoef[index] = outCoef[index] + this.coefficientsNZ[i] * input.coefficientsNZ[j];
+			}
+		}
+		
+		Polynomial output = new Polynomial(outCoef, outExpo);
+		return output;
+	}
+	
 }
